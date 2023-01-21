2d958984e7430ca4b2dab6e0b7553051b0c3b8dd
rbltone <123043582+rbltone@users.noreply.github.com>
2023-01-20 17:45:08 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿// this is a single line comment
+/*this is a multi line comment
+ * 
+ * Author: Anthony Perez
+ * Course: COMP-003A-L01
+ * Purpose: Code for Assignment 1
+ */
+namespace COMP003A.Assignment1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("100 Things About Me");
+        Console.WriteLine("1. My name is Anthony");
+        Console.WriteLine("2. I am 23 years old");
+        Console.WriteLine("3. I am currently attenting WHCCL");
+        Console.WriteLine("4. I aspire to get a BS in Computer Science");
+        Console.WriteLine("5. I have brown hair");
+        Console.WriteLine("6. I have brown eyes");
+        Console.WriteLine("7. I used to play football");
+        Console.WriteLine("8. I used to play basketball");
+        Console.WriteLine("9. I used to play baseball");
+        Console.WriteLine("10. I like to watch movies");
+        Console.WriteLine("11. I like to listen to music");
+        Console.WriteLine("12. I have 1 dog as a pet");
+        Console.WriteLine("13. I have a cat as a pet");
+        Console.WriteLine("14. I have 2 siblings");
+        Console.WriteLine("15. I have 2 sisters");
+        Console.WriteLine("16. I like to drink coffee");
+        Console.WriteLine("17. I drive a Subaru");
+        Console.WriteLine("18. I have long hair");
+        Console.WriteLine("19. My favorite color is green");
+        Console.WriteLine("20. I drink 5 bottles of water a day");
+        Console.WriteLine("21. i do not drink soda");
+        Console.WriteLine("22. I like to eat pizza");
+        Console.WriteLine("23. My favorite movie used to be End of Watch");
+        Console.WriteLine("24. I cannot say that I have a favorite movie at the moment");
+        Console.WriteLine("25. I am eating a cream cheese danish as I type this");
+        Console.WriteLine("26. I am drinking a black americano as I type this");
+        Console.WriteLine("27. I like to eat chicken");
+        Console.WriteLine("28. I like to eat rice");
+        Console.WriteLine("29. I do not like to eat fast food");
+        Console.WriteLine("30. I like action movies");
+        Console.WriteLine("31. I like thriller movies");
+        Console.WriteLine("32. I like mystery movies");
+        Console.WriteLine("33. I have a carbon fiber hood");
+        Console.WriteLine("34. I have a carbon fiber trunk");
+        Console.WriteLine("35. I do my own oil changes on my car");
+        Console.WriteLine("36. I am listening to music as i type this");
+        Console.WriteLine("37. I am sitting in a cafe as I type this");
+        Console.WriteLine("38. I have blurry eyesight");
+        Console.WriteLine("39. I need a new perscription");
+        Console.WriteLine("40. I need to get new reading frames");
+        Console.WriteLine("41. I used to have short hair");
+        Console.WriteLine("42. I recently repaired my hood and trunk");
+        Console.WriteLine("43. I am planning on watching the superbowl");
+        Console.WriteLine("44. i watched the world cup this year");
+        Console.WriteLine("45. I have two backpacks");
+        Console.WriteLine("46. Both of my backpacks are black");
+        Console.WriteLine("47. I have pins on both of my backpacks");
+        Console.WriteLine("48. I have a cut on my hand");
+        Console.WriteLine("49. I have a goldfish");
+        Console.WriteLine("50, I used to have a lizard");
+        Console.WriteLine("51. I want to own a bird");
+        Console.WriteLine("52. I like the Grateful Dead");
+        Console.WriteLine("53. I like Michael Jackson");
+        Console.WriteLine("54. I like SZA");
+        Console.WriteLine("55. I like Frank Ocean");
+        Console.WriteLine("56. I like Game of Thrones");
+        Console.WriteLine("57. My favorite NFL team used to be the Minnesota Vikings");
+        Console.WriteLine("58. My favorite basketball team used to be the LA Lakers");
+        Console.WriteLine("59. I once went to a 49ers game where they played the Vikings");
+        Console.WriteLine("60. My number when I played sports was 28");
+        Console.WriteLine("61. I enjoy eating different types of food");
+        Console.WriteLine("62. My current favorite type of food is Thai");
+        Console.WriteLine("63. I like spicy food");
+        Console.WriteLine("64. One of my favorite foods are tamales");
+        Console.WriteLine("65. I like to drink my coffee black because it is less calories");
+        Console.WriteLine("66. I do not like to consume much sugar");
+        Console.WriteLine("67. I have stickers on my laptop");
+        Console.WriteLine("68. I need to charge my laptop");
+        Console.WriteLine("69. This is my first coding class");
+        Console.WriteLine("70. I want to improve my typing WPM");
+        Console.WriteLine("71. I like room temperature coffee");
+        Console.WriteLine("72. I like Mac Miller");
+        Console.WriteLine("73. I like art");
+        Console.WriteLine("74. I have a collection of vinyl");
+        Console.WriteLine("75. I like blueberries");
+        Console.WriteLine("76. I like watermelon");
+        Console.WriteLine("77. I like toast and cream cheese");
+        Console.WriteLine("78. I like watching F1");
+        Console.WriteLine("79. I like watching live performances");
+        Console.WriteLine("80. I like watching theatre plays");
+        Console.WriteLine("81. I like collecting clothes");
+        Console.WriteLine("82. I like eating desserts");
+        Console.WriteLine("83. I like to eat macaroons");
+        Console.WriteLine("84. I like trying baklava");
+        Console.WriteLine("85. I like Soul");
+        Console.WriteLine("86. I like Thrash Metal");
+        Console.WriteLine("87. I like Rap");
+        Console.WriteLine("88. I like Underground Rap");
+        Console.WriteLine("89. I like Classic Rock");
+        Console.WriteLine("90. I like Punk");
+        Console.WriteLine("91. I like trying different types of cheese");
+        Console.WriteLine("92. I like to watch ONEFC");
+        Console.WriteLine("93. I like peaches");
+        Console.WriteLine("94. I like apples");
+        Console.WriteLine("95. I like cheesecake");
+        Console.WriteLine("96. I like traveling");
+        Console.WriteLine("97. I do not like public relations");
+        Console.WriteLine("98. I like reading");
+        Console.WriteLine("99. I used to collect sports cards");
+        Console.WriteLine("100. I like to make latte art");
+    }
+
+
+}
+
